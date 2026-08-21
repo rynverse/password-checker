dd21b4427966f1e27663ff642c6888ad69466d71
rynverse <nmagno@proton.me>
2026-08-21 09:18:14 +0100

    Create main.py
    First code commit

---
+++ main.py
@@ -0,0 +1,19 @@
+## Take input from user
+
+## Split entered input into a substring of arrays
+
+## Compare to a "dictionary" of values
+## e.g Letter = 1, Symbol = 2, Exceptions = 3
+
+
+## Then, save results to a file by creating a directory called resultss
+
+def checkPassword(userPassword):
+    # Debug Code
+    for i in range(len(userPassword)):
+        print(userPassword[i])
+
+
+## Main Code
+checkPassword(input("Please enter password to be tested: "))
+
